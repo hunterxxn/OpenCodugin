9d3156d97366b7f08260a9384387cd744f9921c4
GitHub Action <action@github.com>
2026-05-20 13:57:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/hunterxxn/opencodugin/MyBundle.kt
+++ src/main/kotlin/com/github/hunterxxn/opencodugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hunterxxn.opencodugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hunterxxn/opencodugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/hunterxxn/opencodugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.hunterxxn.opencodugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
